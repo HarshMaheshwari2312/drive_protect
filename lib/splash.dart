b52e7ad9a3ec8e2fae2bc3b2c09edec7e20670f4
Harsh <driveprotect247@gmail.com>
2023-08-26 00:58:08 +0530

    Initial commit

---
+++ lib/splash.dart
@@ -0,0 +1,70 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DriveProtectApp());
+}
+
+class DriveProtectApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Drive Protect',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(), // Replace with your home screen widget
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Simulate a delay to mimic loading time
+    Timer(Duration(seconds: 5), () {
+      // After the delay, navigate to the main screen
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MainScreen()), // Replace with your main screen widget
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Your splash screen logo or content
+            Image.asset('assets/asset/logo.jpg'), // Replace with your logo asset
+            SizedBox(height: 20),
+            CircularProgressIndicator(), // Loading indicator
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Drive Protect'),
+      ),
+      body: Center(
+        child: Text('Main Screen Content'),
+      ),
+    );
+  }
+}
