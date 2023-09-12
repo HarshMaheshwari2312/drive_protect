d01d512087391c950458c6f4a39542199b8060e0
Harsh <driveprotect247@gmail.com>
2023-09-12 15:31:42 +0530

    meters adjusted

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -4,65 +4,94 @@ import 'package:syncfusion_flutter_gauges/gauges.dart';
 class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Calculate a base size based on the screen width
+    final double baseSize = MediaQuery.of(context).size.width * 0.4;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Dashboard Page"),
       ),
-      body: Stack(
+      body: ListView(
         children: [
-          Positioned(
-            top: 10,
-            left: 0,
-            child: Container(
-              width: 200,
-              height: 200,
-              child: SfRadialGauge(
-                enableLoadingAnimation: true,
-                animationDuration: 4500,
-                axes: <RadialAxis>[
-                  RadialAxis(
-                    minimum: 0,
-                    maximum: 150,
-                    ranges: <GaugeRange>[
-                      GaugeRange(startValue: 0, endValue: 50, color: Colors.green),
-                      GaugeRange(startValue: 50, endValue: 100, color: Colors.orange),
-                      GaugeRange(startValue: 100, endValue: 150, color: Colors.red),
-                    ],
-                    pointers: <GaugePointer>[
-                      NeedlePointer(value: 100, enableAnimation: true),
-                    ],
-                  ),
-                ],
+          Column(
+            children: [
+              Container(
+                height: baseSize * 2, // Adjust height as needed
+                alignment: Alignment.center,
+                child: SfRadialGauge(
+                  enableLoadingAnimation: true,
+                  animationDuration: 4500,
+                  axes: <RadialAxis>[
+                    RadialAxis(
+                      minimum: 0,
+                      maximum: 180,
+                      ranges: <GaugeRange>[
+                        GaugeRange(startValue: 0, endValue: 60, color: Colors.green),
+                        GaugeRange(startValue: 60, endValue: 120, color: Colors.orange),
+                        GaugeRange(startValue: 120, endValue: 180, color: Colors.red),
+                      ],
+                      pointers: <GaugePointer>[
+                        NeedlePointer(value: 100, enableAnimation: true),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
+              Text("Speed", style: TextStyle(fontSize: 20)), // Add the text "Speed" here
+            ],
           ),
-          Positioned(
-            top: 10,
-            right: 0, // Position it to the right side
-            child: Container(
-              width: 200,
-              height: 200,
-              child: SfRadialGauge(
-                enableLoadingAnimation: true,
-                animationDuration: 4500,
-                axes: <RadialAxis>[
-                  RadialAxis(
-                    minimum: 0,
-                    maximum: 100, // Adjust max value as needed
-                    ranges: <GaugeRange>[
-                      GaugeRange(startValue: 0, endValue: 25, color: Colors.green),
-                      GaugeRange(startValue: 25, endValue: 50, color: Colors.orange),
-                      GaugeRange(startValue: 50, endValue: 75, color: Colors.yellow),
-                      GaugeRange(startValue: 75, endValue: 100, color: Colors.red),
-                    ],
-
-                    pointers: <GaugePointer>[
-                      NeedlePointer(value: 50, enableAnimation: true),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Arrange meters evenly
+            children: [
+              Expanded(
+                child: Container(
+                  height: baseSize, // Use the base size
+                  child: SfRadialGauge(
+                    enableLoadingAnimation: true,
+                    animationDuration: 4500,
+                    axes: <RadialAxis>[
+                      RadialAxis(
+                        minimum: 0,
+                        maximum: 100, // Adjust max value as needed
+                        ranges: <GaugeRange>[
+                          GaugeRange(startValue: 0, endValue: 25, color: Colors.green),
+                          GaugeRange(startValue: 25, endValue: 50, color: Colors.orange),
+                          GaugeRange(startValue: 50, endValue: 75, color: Colors.yellow),
+                          GaugeRange(startValue: 75, endValue: 100, color: Colors.red),
+                        ],
+                        pointers: <GaugePointer>[
+                          NeedlePointer(value: 50, enableAnimation: true),
+                        ],
+                      ),
                     ],
                   ),
-                ],
+                ),
               ),
-            ),
+              Expanded(
+                child: Container(
+                  height: baseSize, // Use the base size
+                  child: SfRadialGauge(
+                    enableLoadingAnimation: true,
+                    animationDuration: 4500,
+                    axes: <RadialAxis>[
+                      RadialAxis(
+                        minimum: 0,
+                        maximum: 100, // Adjust max value as needed
+                        ranges: <GaugeRange>[
+                          GaugeRange(startValue: 0, endValue: 25, color: Colors.green),
+                          GaugeRange(startValue: 25, endValue: 50, color: Colors.orange),
+                          GaugeRange(startValue: 50, endValue: 75, color: Colors.yellow),
+                          GaugeRange(startValue: 75, endValue: 100, color: Colors.red),
+                        ],
+                        pointers: <GaugePointer>[
+                          NeedlePointer(value: 50, enableAnimation: true),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
